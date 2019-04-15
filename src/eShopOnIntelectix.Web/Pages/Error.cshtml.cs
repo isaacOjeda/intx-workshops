474e498c57941aa7c150a690769d4e61a4c5e385
Isaac Ojeda <isaac.ojeda@intelectix.com>
2019-04-15 17:31:49 -0600

    Minor updates

---
+++ src/eShopOnIntelectix.Web/Pages/Error.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace eShopOnIntelectix.Web.Pages
+{
+    public class ErrorModel : PageModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
